98a6f25ffd4ca3c1a9db88d268e318188c6c5c7d
Kentleigh English <kentleigh@gmx.com>
2019-06-11 23:21:36 +0100

    Add YouTube title fetch to queuelib

--- site/server/queuelib.js
+++ site/server/queuelib.js
@@ -2,16 +2,36 @@
 const Datastore = require('nedb');
 const db = new Datastore({ filename: './store/database.db', autoload: true });
 const debug = require('debug')('linkq:queuelib');
+const { gapi } = require('config');
+const { youtube_v3: youtube } = require('googleapis');
+
+const yt = new youtube.Youtube({
+	auth: gapi.key
+});
 
 var queue = [];
 
 const ytRegex = /.*(youtube.com\/watch\?v=|youtu.be\/)/;
 
+const fetchYoutubeTitle = async (id) => {
+	const { status, data: { items = [] } } = await yt.videos.list({ part: 'snippet', id });
+
+	if (status === 200 && items.length) {
+		return items[0].snippet.title;
+	}
+
+	return null;
+}
+
 const add = async (url) => {
+	const videoId = url.replace(ytRegex, '');
+
+	const title = await fetchYoutubeTitle(videoId);
+
 	db.insert({
 		index: queue.length,
-		playing: false,
-		videoId: url.replace(ytRegex, '')
+		videoId,
+		title
 	}, err => {
 		if (!err) {
 			return true;
